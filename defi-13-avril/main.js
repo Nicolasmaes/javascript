9dfe90d00949f7afe6262c7d71d74803f4588380
Nicolas <n.maes@codeur-online>
2021-04-13 11:03:12 +0200

    ajout du défi-13-avril mot de passe à afficher

---
+++ defi-13-avril/main.js
@@ -0,0 +1,28 @@
+const input = document.getElementById("input") ;
+const display = document.getElementById("display");
+
+document.getElementById("checkbox").onmousedown = function() {mouseDown()};
+document.getElementById("checkbox").onmouseup = function() {mouseUp()};
+
+function mouseDown() {
+     input.type = 'text';
+     display.innerHTML = 'Hide';
+     checkbox.checked=true;
+}
+function mouseUp() {
+     input.type = 'password';
+     display.innerHTML = 'Show';
+};
+
+// manière avec simple clic sur la checkbox 
+
+/*document.getElementById('checkbox').addEventListener('click', function() {
+     if(checkbox.checked){
+     input.type = 'text';
+     display.innerHTML = 'Hide';
+     }
+     else{
+     input.type = 'password';
+     display.innerHTML = 'Show';
+     }
+     })*/
