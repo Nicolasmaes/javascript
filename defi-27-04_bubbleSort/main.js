9d52b27aba901f68f7f99b8b5856f4e0a32787ef
Nicolas <n.maes@codeur-online>
2021-04-27 13:23:39 +0200

    ajout du défi bubbleSort corrigé

--- defi-27-04_bubbleSort/main.js
+++ defi-27-04_bubbleSort/main.js
@@ -4,8 +4,7 @@ let array = [];
 for (let i = 0; i <100; i++){
 let number = Math.floor(Math.random()*100);
 array.push (number);
-draw();
-sort();
+
 }
 
 function draw() {
@@ -14,11 +13,23 @@ function draw() {
        let div = document.createElement('div');
        div.style.height = x+'px';
        chart.appendChild(div);
-
    })
 }
-
-function sort() {
+/* function sort() {
         array.sort(function(a, b){return a-b});
-    }
+    } */
 
+function bubbleSort () {
+    for(let i = 0; i < array.length; i++){
+        for(let j = 0; j < ( array.length - i -1 ); j++){
+            if(array[j] > array[j+1]){
+            let temp = array[j];
+            array[j] = array[j + 1];
+            array[j+1] = temp;
+            }
+            draw();
+        }
+        }
+    }
+    
+bubbleSort();
